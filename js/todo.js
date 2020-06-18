289186497f92885461b501e2513493feacf8d948
ReeceB4 <bucktonreece@gmail.com>
2020-06-18 13:23:03 +0200

    styling

--- js/todo.js
+++ js/todo.js
@@ -1,53 +1,61 @@
-function cleanup() {
+function cleanUp() {
 
+    // WRAP LIST TEXT IN A P, AND APPLY FUNCTIONALITY TABS
     $(".panel li")
         .wrapInner("<p>")
-        .prepend("<div class=\"btn-group\"><button type=\"button\" class=\"btn btn-primary check\"><span class=\"glyphicon glyphicon-ok\"></span> </button> <button type=\"button\" class=\"btn btn-primary del\"> <span class=\"glyphicon glyphicon-remove\"></span></button></div>");
-
-        //check if ListItemID and strike through when needed 
+        .prepend("<div class=\"btn-group\"><button type=\"button\" class=\"btn btn-primary check\"><span class=\"glyphicon glyphicon-ok\"></span></button><button type=\"button\" class=\"btn btn-primary del\"><span class=\"glyphicon glyphicon-remove\"></span></button></div>");
+    
+    //check if ListItemDone and render strike-through when needed
     $.ajax({
         type: "POST",
-        url: "inc/renderStrike.php",
+        url:"inc/renderStrike.php",
         async: true,
         dataType: "json",
         success: function( msg ) {
-            strikeArrLength = msg.length;
-            for (i =0; i < strikeArrLength; i++) {
-                    console.log(msg[i]);
-                if(msg[i] > 0){
-                    $(".panel li:eq(" + i + ")").find("p").css("text-decoration","line-through");
-                }else{
-                    $(".panel li:eq(" + i + ")").find("p").css("text-decoration","none");
-                }
+        strikeArrLength = msg.length;
+        for (i = 0; i < strikeArrLength; i++) {
+                console.log(msg[i]);
+            if(msg[i] > 0){
+            $(".panel li:eq(" + i + ")").find("p").css("text-decoration","line-through");
+            }else{
+            $(".panel li:eq(" + i + ")").find("p").css("text-decoration","none");	
             }
-        }//success function
+        }//end for
+        }//end success function
     });
-};//cleanup function
-
-//check status of ListItemID and strike through if needed 
-$("li").on("click", "check", function() {
-    var checkNum = $(this).parent().parent().index();
-
-    $.ajax({
+    
+    };
+    
+    $("li").on("click", ".check", function() {
+       //console.log("click");
+       
+       var checkNum = $(this).parent().parent().index();   
+    
+       $.ajax({
         type: "POST",
-        url: "inc/done.php",
+        url:"inc/done.php",
         data: "val=" + checkNum,
         async: true,
         dataType: "text",
         success: function( data ) {
-            if(data > 0){
-                $(".panel li:eq(" + checkNum + ")").find("p").css("text-decoration","line-through");
-            }else{
-                $(".panel li:eq(" + checkNum + ")").find("p").css("text-decoration","none");
-            }
+        if(data > 0){
+            $(".panel li:eq(" + checkNum + ")").find("p").css("text-decoration","line-through");
+        }else{
+            $(".panel li:eq(" + checkNum + ")").find("p").css("text-decoration","none");	
         }
-    });//ajax call
-});
-
-//delete list item
-$("li").on("click", ".del", function() {
-    //remove entry
-    $(this).parent().parent().fadeOut("fast");
-    var checkNum = $(this).parent().parent().index();
-    $.post('inc/delete.php', 'val=' + checkNum);
-});
+        }
+    }); 
+       //console.log();
+           
+    });
+    
+    $("li").on("click", ".del", function() {
+       //console.log("del");
+       $(this).parent().parent().fadeOut("fast");
+       var checkNum = $(this).parent().parent().index(); 
+       $.post('inc/delete.php', 'val=' + checkNum);
+    });
+    
+    
+    
+    
